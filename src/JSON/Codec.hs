b57986d06a8bb1c2bd5859508031be7d42b775fd
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-10-02 21:01:33 +0200

    object codec

--- src/JSON/Codec.hs
+++ src/JSON/Codec.hs
@@ -1,26 +1,31 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module JSON.Codec where
 
-import Data.Attoparsec.ByteString.Char8 (char, hexadecimal)
+import Control.Monad
+import Data.Attoparsec.ByteString.Char8 (char)
 import Data.Attoparsec.ByteString.Lazy
+import qualified Data.ByteString.Builder as BB
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.Char as Char
+import Data.String
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Builder as LTB
 import Data.Word
+import Text.Printf
 
 -- TODO find a better name for this?
 data Codec input output where
   NullCodec :: Codec () ()
   BoolCodec :: Codec Bool Bool
   StringCodec :: Codec Text Text
+  ObjectCodec :: ObjectCodec value value -> Codec value value
   PureCodec :: output -> Codec input output
-  ApCodec :: Codec input (output -> newOutput) -> Codec input output -> Codec input newOutput
   BimapCodec :: (oldOutput -> newOutput) -> (newInput -> oldInput) -> Codec oldInput oldOutput -> Codec newInput newOutput
 
 instance Functor (Codec input) where
@@ -35,15 +40,42 @@ comapCodec g = BimapCodec id g
 bimapCodec :: (oldOutput -> newOutput) -> (newInput -> oldInput) -> Codec oldInput oldOutput -> Codec newInput newOutput
 bimapCodec = BimapCodec
 
-instance Applicative (Codec input) where
-  pure = pureCodec
-  (<*>) = apCodec
+data ObjectCodec input output where
+  KeyCodec :: Text -> Codec input output -> ObjectCodec input output
+  PureObjectCodec :: output -> ObjectCodec input output
+  BimapObjectCodec :: (oldOutput -> newOutput) -> (newInput -> oldInput) -> ObjectCodec oldInput oldOutput -> ObjectCodec newInput newOutput
+  ApObjectCodec :: ObjectCodec input (output -> newOutput) -> ObjectCodec input output -> ObjectCodec input newOutput
 
-pureCodec :: output -> Codec input output
-pureCodec = PureCodec
+fmapObjectCodec :: (oldOutput -> newOutput) -> ObjectCodec input oldOutput -> ObjectCodec input newOutput
+fmapObjectCodec f = BimapObjectCodec f id
 
-apCodec :: Codec input (output -> newOutput) -> Codec input output -> Codec input newOutput
-apCodec = ApCodec
+comapObjectCodec :: (newInput -> oldInput) -> ObjectCodec oldInput output -> ObjectCodec newInput output
+comapObjectCodec g = BimapObjectCodec id g
+
+bimapObjectCodec :: (oldOutput -> newOutput) -> (newInput -> oldInput) -> ObjectCodec oldInput oldOutput -> ObjectCodec newInput newOutput
+bimapObjectCodec = BimapObjectCodec
+
+object :: ObjectCodec value value -> Codec value value
+object = ObjectCodec
+
+instance Functor (ObjectCodec input) where
+  fmap = fmapObjectCodec
+
+instance Applicative (ObjectCodec input) where
+  pure = pureObjectCodec
+  (<*>) = apObjectCodec
+
+instance HasCodec output => IsString (ObjectCodec output output) where
+  fromString s = KeyCodec (fromString s) codec
+
+(.=) :: ObjectCodec oldInput output -> (newInput -> oldInput) -> ObjectCodec newInput output
+(.=) = flip comapObjectCodec
+
+pureObjectCodec :: output -> ObjectCodec input output
+pureObjectCodec = PureObjectCodec
+
+apObjectCodec :: ObjectCodec input (output -> newOutput) -> ObjectCodec input output -> ObjectCodec input newOutput
+apObjectCodec = ApObjectCodec
 
 boolCodec :: Codec Bool Bool
 boolCodec = BoolCodec
@@ -59,9 +91,19 @@ codecParser = go
       NullCodec -> nullParser
       BoolCodec -> boolParser
       StringCodec -> stringParser
-      PureCodec output -> pure output
-      ApCodec functionCodec inputCodec -> go functionCodec <*> go inputCodec
+      ObjectCodec oc -> objectCodecParser oc
+      PureCodec v -> pure v
       BimapCodec f _ c -> f <$> go c
+
+objectCodecParser :: ObjectCodec void output -> Parser output
+objectCodecParser = go
+  where
+    go :: ObjectCodec void output -> Parser output
+    go = \case
+      KeyCodec _ _ -> undefined -- TODO
+      PureObjectCodec output -> pure output
+      ApObjectCodec functionCodec inputCodec -> go functionCodec <*> go inputCodec
+      BimapObjectCodec f _ c -> f <$> go c
 
 nullParser :: Parser ()
 nullParser = () <$ string "null"
@@ -75,7 +117,7 @@ boolParser =
 
 stringParser :: Parser Text
 stringParser = do
-  char '\"'
+  void $ char '\"'
   nextChar <- anyWord8
   -- TODO split off a lazy text builder?
   LT.toStrict . LTB.toLazyText <$> go nextChar
@@ -114,25 +156,24 @@ stringParser = do
     goUnicode = do
       let hexChar = do
             w <- anyWord8
-            pure $
-              Char.digitToInt $ case w of
-                48 -> '0'
-                49 -> '1'
-                50 -> '2'
-                51 -> '3'
-                52 -> '4'
-                53 -> '5'
-                54 -> '6'
-                55 -> '7'
-                56 -> '8'
-                57 -> '9'
-                97 -> 'a'
-                98 -> 'b'
-                99 -> 'c'
-                100 -> 'd'
-                101 -> 'e'
-                102 -> 'f'
-
+            Char.digitToInt <$> case w of
+              48 -> pure '0'
+              49 -> pure '1'
+              50 -> pure '2'
+              51 -> pure '3'
+              52 -> pure '4'
+              53 -> pure '5'
+              54 -> pure '6'
+              55 -> pure '7'
+              56 -> pure '8'
+              57 -> pure '9'
+              97 -> pure 'a'
+              98 -> pure 'b'
+              99 -> pure 'c'
+              100 -> pure 'd'
+              101 -> pure 'e'
+              102 -> pure 'f'
+              _ -> fail "expected a hex char"
       h1 <- hexChar
       h2 <- hexChar
       h3 <- hexChar
@@ -156,3 +197,56 @@ parseMaybe = maybeResult . parseResult
 
 parseEither :: HasCodec output => LB.ByteString -> Either String output
 parseEither = eitherResult . parseResult
+
+render :: HasCodec input => input -> LB.ByteString
+render = renderViaCodec codec
+
+renderViaCodec :: Codec input void -> input -> LB.ByteString
+renderViaCodec c input = BB.toLazyByteString $ codecBuilder c input
+
+codecBuilder :: Codec input void -> input -> BB.Builder
+codecBuilder = flip go
+  where
+    go :: input -> Codec input void -> BB.Builder
+    go input = \case
+      NullCodec -> nullBuilder
+      BoolCodec -> boolBuilder input
+      StringCodec -> stringBuilder input
+      ObjectCodec oc -> objectCodecBuilder oc input
+      PureCodec _ -> mempty
+      BimapCodec _ g oc -> go (g input) oc
+
+objectCodecBuilder :: ObjectCodec input void -> input -> BB.Builder
+objectCodecBuilder = flip go
+  where
+    go :: input -> ObjectCodec input void -> BB.Builder
+    go input = \case
+      KeyCodec _ _ -> undefined -- TODO
+      ApObjectCodec oc1 oc2 -> go input oc1 <> go input oc2
+      PureObjectCodec _ -> mempty
+      BimapObjectCodec _ g oc -> go (g input) oc
+
+nullBuilder :: BB.Builder
+nullBuilder = "null"
+
+boolBuilder :: Bool -> BB.Builder
+boolBuilder = \case
+  True -> "true"
+  False -> "false"
+
+stringBuilder :: Text -> BB.Builder
+stringBuilder = (BB.char7 '"' <>) . (<> BB.char7 '"') . foldMap go . T.unpack
+  where
+    go :: Char -> BB.Builder
+    go = \case
+      '"' -> BB.char7 '\\' <> BB.char7 '"'
+      '\\' -> BB.char7 '\\' <> BB.char7 '\\'
+      '\b' -> BB.char7 '\\' <> BB.char7 'b'
+      '\f' -> BB.char7 '\\' <> BB.char7 'f'
+      '\n' -> BB.char7 '\\' <> BB.char7 'n'
+      '\r' -> BB.char7 '\\' <> BB.char7 'r'
+      '\t' -> BB.char7 '\\' <> BB.char7 't'
+      c
+        | c <= '\127' -> BB.char7 c
+        | c <= '\255' -> BB.char7 '\\' <> BB.string7 (printf "%d" (Char.ord c))
+        | otherwise -> BB.char7 '\\' <> BB.char7 'u' <> BB.string7 (printf "%04x" (Char.ord c))
--- test/JSON/CodecSpec.hs
+++ test/JSON/CodecSpec.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module JSON.CodecSpec (spec) where
 
-import Data.Aeson
+import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as LB
+import Data.GenValidity.Text ()
 import Data.Text (Text)
+import GHC.Generics (Generic)
 import JSON.Codec as JSON
 import Test.Syd
+import Test.Syd.Validity
 
 spec :: Spec
 spec = do
@@ -17,9 +22,12 @@ spec = do
     parseSuccessSpec "true" True
     parseSuccessSpec "false" False
     parseFailSpec @Bool "no"
+    roundtripSpec @Bool
+    equivalentToAesonSpec @Bool
 
   describe "Text" $ do
     parseSuccessSpec @Text "\"Hello world!\"" "Hello world!"
+    parseSuccessSpec @Text "\"\"" ""
     parseSuccessSpec @Text "\"\\\"" "\""
     parseSuccessSpec @Text "\"\\b\"" "\b"
     parseSuccessSpec @Text "\"\\f\"" "\f"
@@ -29,11 +37,43 @@ spec = do
     parseSuccessSpec @Text "\"\\\\\"" "\\"
     parseSuccessSpec @Text "\"\\u1234\"" "\4660"
     parseFailSpec @Text "\"unclosed double quote"
+  -- roundtripSpec @Text
+  -- equivalentToAesonSpec @Text
 
--- equivalentToAesonSpec :: forall output. String -> Spec
--- equivalentToAesonSpec name = describe name $ do
---   it "produces the same output as aeson does" $ do
---     undefined
+  describe "Thing" $
+    roundtripSpec @Thing
+
+data Thing = Thing {thingBool :: !Bool, thingText :: !Text}
+  deriving (Show, Eq, Generic)
+
+instance Validity Thing
+
+instance GenValid Thing where
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
+
+instance HasCodec Thing where
+  codec =
+    object $
+      Thing
+        <$> "bool" .= thingBool
+        <*> "text" .= thingText
+
+equivalentToAesonSpec :: forall value. (Show value, GenValid value, Aeson.ToJSON value, HasCodec value) => Spec
+equivalentToAesonSpec = do
+  it "produces the same output as aeson does" $
+    forAllValid $ \(value :: value) ->
+      let rendered = render value
+          renderedViaAeson = Aeson.encode value
+       in rendered `shouldBe` renderedViaAeson
+
+roundtripSpec :: forall value. (Show value, Eq value, GenValid value, HasCodec value) => Spec
+roundtripSpec = it "roundtrips" $
+  forAllValid $ \expected ->
+    let rendered = render expected
+     in case parseEither @value rendered of
+          Left err -> expectationFailure err
+          Right actual -> context (show rendered) $ actual `shouldBe` expected
 
 parseSuccess :: (Show output, Eq output, HasCodec output) => LB.ByteString -> output -> IO ()
 parseSuccess input expected = case JSON.parseEither input of
